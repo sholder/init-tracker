476fc53b17ef2dbf6830391aad8c98ab42fd52b7
Steve Holder <steve.holder@gmail.com>
2015-12-01 12:08:15 -0800

    initial setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name initTrackerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the initTrackerApp
+ */
+angular.module('initTrackerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name initTrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the initTrackerApp
+ */
+angular.module('initTrackerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

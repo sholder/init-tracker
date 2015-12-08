cec9fe2368a42337395cde82f826a5f0ca181d3b
Steve Holder <steve.holder@gmail.com>
2015-12-07 16:31:14 -0800

    added entity controller unit tests

--- test/spec/controllers/entity.js
+++ test/spec/controllers/entity.js
@@ -2,22 +2,45 @@
 
 describe('Controller: EntityCtrl', function () {
 
+  var EntityCtrl,
+    scope,
+    storageService;
+
   // load the controller's module
   beforeEach(module('initTrackerApp'));
 
-  var EntityCtrl,
-    scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, localStorageService) {
     scope = $rootScope.$new();
+    storageService = localStorageService;
+
+    storageService.set('entities', []);
+
     EntityCtrl = $controller('EntityCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    //expect(EntityCtrl.awesomeThings.length).toBe(3);
+  it('should start with an empty list of entities', function () {
+    expect(scope.allEntities).toEqual([]);
+  });
+
+  it('should add and persist an entity', function() {
+    var palindraEntity = {
+      name: 'Palindra',
+      mod: 4
+    };
+    scope.entity = palindraEntity;
+    scope.addEntity();
+    // Ensure that watches fire.
+    scope.$apply();
+
+    expect(scope.entity).toEqual({});
+    expect(scope.allEntities).toContain(palindraEntity);
+    var persistedEntities = storageService.get('entities');
+    expect(persistedEntities.length).toEqual(1);
+    expect(persistedEntities).toContain(palindraEntity);
   });
 });
